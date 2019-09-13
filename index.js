6a0214d02a00ec64b80a7f84807b0b1e69987b96
unwriter <unwriter@protonmail.com>
2019-09-13 15:05:45 -0400

    error handling

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const spawn = require('child_process').spawn;
-const run = function(stream, filter) {
+const run = function(stream, filter, error) {
   const child = spawn('node_modules/streamjq/bin/jq', [filter]);
   stream.pipe(child.stdin)
+  if (error) child.stdin.on("error", error)
   return child.stdout
 }
 module.exports = run
